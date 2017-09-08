a657b1564c45da26832dfc1fec7c22a71268b872
Maxine Divers <maxinedivers@users.noreply.github.com>
2017-09-08 11:43:13 +0100

    tried third time lucky

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ redis.getClient(function (err, client) {
 
 function init(sessionStore) {
     // auth
-  if (process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === 'heroku') {
+    if (process.env.NODE_ENV && process.env.NODE_ENV.toLowerCase() === 'heroku') {
         var auth = require('express-basic-auth');
 
        function authoriser(user, pass) {
