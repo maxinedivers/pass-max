3a3354d091fc2e922065f089396ad4afe4d7f5e3
Paul Winkler <paul.winkler@homeoffice.gov.uk>
2017-10-16 18:07:57 +0100

    Remove node_modules dir

--- node_modules/hmpo-form-wizard/example/config/index.js
+++
@@ -1,5 +0,0 @@
-var _ = require('underscore');
-module.exports = _.extend({
-    API_URL: 'http://localhost:3000/api',
-    PORT: 3000
-}, process.env);
--- node_modules/hmpo-frontend-toolkit/assets/javascript/vendor/safari-cachebuster.js
+++
@@ -1,5 +0,0 @@
-window.onpageshow = function(event) {
-    if (event.persisted) {
-        window.location.reload();
-    }
-};
